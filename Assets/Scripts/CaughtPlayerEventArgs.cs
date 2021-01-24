439adb650642ed5674dc99ecaf93bee1f9d78a10
Daria Zaytseva <vezuka@gmail.com>
2021-01-24 10:55:26 +0300

    Lesson3

---
+++ Assets/Scripts/CaughtPlayerEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace RollaBall
+{
+    public sealed class CaughtPlayerEventArgs : EventArgs
+    {
+        public Color Color { get; }
+
+        public CaughtPlayerEventArgs(Color color)
+        {
+            Color = color;
+        }
+    }
+}
+
+
